4a5453bac30951bd545dd10cf45123f64c3f07f3
Stone <star2018@outlook.com>
2021-02-02 16:23:36 +0800

    refactor: optimize

--- README.md
+++ README.md
@@ -9,14 +9,18 @@ const { addBefore, addAfter, add, find } = require('@ices/use-loader')
 const matched = find(webpackConfig, 'file-loader')
 
 // add new loader config to the slibing position after the file-loader
-const added = add(webpackConfig, 'file-loader', newRuleConfig, (index) => index + 1)
+add(webpackConfig, 'file-loader', newRuleConfig, (index) => index + 1)
 
 // add new loader config to the slibing position before the file-loader
-const added = addBefore(webpackConfig, 'file-loader', newRuleConfig)
+addBefore(webpackConfig, 'file-loader', newRuleConfig)
 
 // add new loader config to the slibing position after the file-loader
-const added = addAfter(webpackConfig, 'file-loader', newRuleConfig)
+addAfter(webpackConfig, 'file-loader', newRuleConfig)
 
 // use customize matcher
-find(webpackConfig, (absolutePath) => absolutePath === '/some/path/loader.js')
+find(webpackConfig, ({ name }) => name === 'file-loader')
 ```
+
+### API
+
+See dist/index.d.ts
--- src/index.ts
+++ src/index.ts
@@ -2,114 +2,177 @@ import * as path from 'path'
 import * as fs from 'fs'
 import type webpack from 'webpack'
 
-type RuleLoaderItem = {
-  parent: webpack.RuleSetRule[] | webpack.RuleSetUseItem[]
+interface RuleLoaderItem {
   loader: string
-  rule: webpack.RuleSetRule | webpack.RuleSetUseItem
   index: number
+  isUseItem: boolean
   isOneOf: boolean
-}
-type Matcher = (
-  loaderPath: string,
-  rule: webpack.RuleSetRule | webpack.RuleSetUseItem,
-  rules: webpack.RuleSetRule[] | webpack.RuleSetUseItem[]
-) => boolean
-type MatchOptions = {
-  rules: webpack.RuleSetRule[] | webpack.RuleSetUseItem[]
-  rule: webpack.RuleSetRule | webpack.RuleSetUseItem
-  use: webpack.RuleSetUse
-  index: number
-  match: Matcher
+  rule: webpack.RuleSetRule
+  siblings: webpack.RuleSetRule[] | webpack.RuleSetUseItem[]
 }
 
-type PositionHandler = (index: number, isOneOf: boolean, length: number) => number
+type FindMatcher = (item: RuleLoaderItem) => boolean
+
+type MatchOptions = {
+  use: webpack.RuleSetUse | undefined
+  index: number
+  match: FindMatcher
+  rule: webpack.RuleSetRule
+  siblings: webpack.RuleSetRule[] | webpack.RuleSetUseItem[]
+}
+
+export interface MatchParameter extends RuleLoaderItem {
+  name: string
+}
+
+export type Matcher = (item: MatchParameter) => boolean
+
+export type PositionHandler = (
+  index: number,
+  length: number,
+  isUseItem: boolean,
+  isOneOf: boolean
+) => number
+
+export type NewRule =
+  | webpack.RuleSetRule
+  | webpack.RuleSetUseItem
+  | ((isUseItem: boolean, isOneOf: boolean) => webpack.RuleSetRule | webpack.RuleSetUseItem)
 
 /**
  * Find the rules that has an loader matched.
- * @param config
- * @param match
+ * @param config webpack configuration
+ * @param match a function to match the loader
  */
 export function find(config: webpack.Configuration, match: string | Matcher) {
   const rules = config?.module?.rules
   if (!Array.isArray(rules)) {
     return []
   }
+
   if (typeof match === 'string') {
-    const loader = resolveLoaderPath(match)
-    match = (loaderPath: string) => isSameLoader(loaderPath, loader)
+    const loaderPath = resolveLoaderPath(match)
+    const loaderName = getPackageName(loaderPath)
+    match = ({ loader, name }) => isSamePath(loaderPath, loader) || isSamePackage(loaderName, name)
   } else if (typeof match !== 'function') {
     throw new Error('Arguments error: second argument is not a string or function')
   }
-  return findLoaderRule(rules, (loaderPath: string, rule, rules) =>
-    (match as Matcher)(resolveLoaderPath(loaderPath), rule, rules)
-  )
+
+  return findLoaderRule(rules, ({ loader, ...rest }) => {
+    const absolutePath = resolveLoaderPath(loader)
+    const matcher = match as Matcher
+    return matcher({ ...rest, loader: absolutePath, name: getPackageName(absolutePath) })
+  })
 }
 
 /**
  * Match the rules that has an loader matched, then add the new rule to the position inside matched rule list.
- * @param config
- * @param matcher
- * @param newRule
- * @param position
+ * @param config webpack config
+ * @param matcher a function to match the loader
+ * @param newRule a rule config to add
+ * @param position if position handler return -1, this rule will not be add
  */
 export function add(
   config: webpack.Configuration,
   matcher: string | Matcher,
-  newRule: any,
+  newRule: NewRule,
   position: PositionHandler
 ) {
   const matched = find(config, matcher)
   if (matched.length) {
-    for (const { parent, index, isOneOf } of matched) {
-      parent.splice(position(index, isOneOf, parent.length), 0, newRule)
+    for (const { siblings, index, isUseItem, isOneOf } of matched) {
+      const pos = getPosition(position, index, siblings.length, isUseItem, isOneOf)
+      if (pos !== -1) {
+        siblings.splice(pos, 0, getNewRule(newRule, isUseItem, isOneOf) as any)
+      }
     }
-    return true
+  } else {
+    const { module = { rules: [] } } = config
+    const { rules } = module
+    if (!config.module) {
+      config.module = module
+    }
+    const pos = getPosition(position, -1, rules.length, false, false)
+    if (pos !== -1) {
+      rules.splice(pos, 0, getNewRule(newRule, false, false) as any)
+    }
   }
-  return false
 }
 
 export default add
 
 /**
  * Add rule config before the rule that matched the loader.
- * @param config
- * @param match
- * @param newRule
+ * If there is no matched, the rule is added to the begin of the rules array.
+ * @param config webpack configuration
+ * @param match a function to match the loader
+ * @param newRule a rule config to add
  */
-export function addBefore(config: webpack.Configuration, match: string | Matcher, newRule: any) {
+export function addBefore(
+  config: webpack.Configuration,
+  match: string | Matcher,
+  newRule: NewRule
+) {
   return add(config, match, newRule, (x) => (x === -1 ? 0 : x))
 }
 
 /**
  * Add rule config after the rule that matched the loader.
- * @param config
- * @param match
- * @param newRule
+ * If there is no matched, the rule is added to the end of the rules array.
+ * @param config webpack configuration
+ * @param match a function to match the loader
+ * @param newRule a rule config to add
  */
-export function addAfter(config: webpack.Configuration, match: string | Matcher, newRule: any) {
-  return add(config, match, newRule, (...args) => (args[0] === -1 ? args[2] : args[0] + 1))
+export function addAfter(config: webpack.Configuration, match: string | Matcher, newRule: NewRule) {
+  return add(config, match, newRule, (...args) => (args[0] === -1 ? args[1] : args[0] + 1))
 }
 
-function findLoaderRule(rules: webpack.RuleSetRule[], match: Matcher, isOneOf = false) {
+function getPosition(handler: PositionHandler, ...args: Parameters<PositionHandler>) {
+  const pos = +handler(...args)
+  const length = args[1]
+  if (Number.isNaN(pos) || /\./.test(`${pos}`) || pos < -1 || pos > length) {
+    throw new Error(
+      `Position handler must return a integer value of number between -1 and ${length}`
+    )
+  }
+  return pos
+}
+
+function getNewRule(newRule: NewRule, isUseItem: boolean, isOneOf: boolean) {
+  const rule = typeof newRule === 'function' ? newRule(isUseItem, isOneOf) : newRule
+  if (isUseItem) {
+    const useItem = rule as webpack.RuleSetUseItem
+    const type = typeof useItem
+    if (type !== 'string' && type !== 'object' && type !== 'function') {
+      throw new Error(`Rule must as a type of webpack.RuleSetUseItem`)
+    }
+    if (type === 'object' && typeof (useItem as any).loader !== 'string') {
+      throw new Error(`RuleSetLoader must has an property of loader that is type of string`)
+    }
+  } else {
+    const ruleItem = rule as webpack.RuleSetRule
+    if (!ruleItem || typeof ruleItem !== 'object') {
+      throw new Error(`RuleSetRule must be an object`)
+    }
+  }
+  return rule
+}
+
+function findLoaderRule(rules: webpack.RuleSetRule[], match: FindMatcher, isOneOf = false) {
   const matched = [] as RuleLoaderItem[]
   for (let index = 0; index < rules.length; index++) {
-    const rule = rules[index] as any
+    const rule = rules[index]
     if (!rule) {
       continue
     }
-    for (const use of ['loader', 'loaders', 'use']) {
-      matched.push(
-        ...matchRuleSet(
-          { use: rule[use] as webpack.RuleSetUse, rules, match, rule, index },
-          isOneOf
-        )
-      )
+    for (const use of [rule.loader, rule.loaders, rule.use]) {
+      matched.push(...matchRuleSet({ use, siblings: rules, match, rule, index }, isOneOf))
     }
     if (Array.isArray(rule.oneOf)) {
       matched.push(...findLoaderRule(rule.oneOf, match, true))
     }
     if (Array.isArray(rule.rules)) {
-      matched.push(...findLoaderRule(rule.rules, match))
+      matched.push(...findLoaderRule(rule.rules, match, false))
     }
   }
 
@@ -117,7 +180,7 @@ function findLoaderRule(rules: webpack.RuleSetRule[], match: Matcher, isOneOf = 
 }
 
 function matchRuleSet(options: MatchOptions, isOneOf: boolean) {
-  const { rules, rule, use, index, match } = options
+  const { siblings, rule, use, index, match } = options
   const matched = [] as RuleLoaderItem[]
   if (!use || typeof use === 'function') {
     return matched
@@ -128,59 +191,59 @@ function matchRuleSet(options: MatchOptions, isOneOf: boolean) {
       const useItem = use[index]
       if (typeof useItem === 'string') {
         // loader
-        const rule = { loader: useItem }
-        if (match(useItem, rule, use)) {
-          matched.push({
-            rule,
-            index,
-            parent: use,
-            loader: useItem,
-            isOneOf: false,
-          })
+        const item = {
+          rule,
+          index,
+          loader: useItem,
+          isUseItem: true,
+          isOneOf: false,
+          siblings: use,
+        }
+        if (match({ ...item })) {
+          matched.push(item)
         }
       } else if (typeof useItem === 'object') {
         // RuleSetLoader
         const ruleSetLoader = useItem.loader
-        if (typeof ruleSetLoader === 'string' && match(ruleSetLoader, useItem, use)) {
-          matched.push({ parent: use, rule: useItem, loader: ruleSetLoader, index, isOneOf: false })
+        if (typeof ruleSetLoader === 'string') {
+          const item = {
+            rule,
+            index,
+            loader: ruleSetLoader,
+            isUseItem: true,
+            isOneOf: false,
+            siblings: use,
+          }
+          if (match({ ...item })) {
+            matched.push(item)
+          }
         }
       }
     }
   } else if (typeof use === 'string') {
     // loader
-    if (match(use, rule, rules)) {
-      matched.push({ parent: rules, rule, loader: use, index, isOneOf })
+    const item = { loader: use, rule, isUseItem: false, isOneOf, siblings, index }
+    if (match({ ...item })) {
+      matched.push(item)
     }
   } else if (typeof use === 'object') {
     // RuleSetLoader
     const ruleSetLoader = use.loader
-    if (typeof ruleSetLoader === 'string' && match(ruleSetLoader, rule, rules)) {
-      matched.push({ parent: rules, rule, loader: ruleSetLoader, index, isOneOf })
+    if (typeof ruleSetLoader === 'string') {
+      const item = { loader: ruleSetLoader, rule, isUseItem: false, isOneOf, siblings, index }
+      if (match({ ...item })) {
+        matched.push(item)
+      }
     }
   }
 
   return matched
 }
 
-function isSameLoader(x: any, y: any) {
-  if (typeof x !== 'string' || typeof y !== 'string') {
-    return false
-  }
-  if (x.replace(/\\/g, '/').toLowerCase() === y.replace(/\\/g, '/').toLowerCase()) {
-    return true
-  }
-  if (path.basename(x) === path.basename(y)) {
-    const xName = getPackageName(x)
-    const yName = getPackageName(y)
-    if (!xName || !yName) {
-      return false
-    }
-    return xName === yName
-  }
-  return false
-}
-
 function getPackageName(file: string) {
+  if (!file) {
+    return ''
+  }
   let dir = file
   do {
     if (path.basename(dir) === 'node_modules') {
@@ -204,4 +267,20 @@ function resolveLoaderPath(loaderPath: string) {
     }
   }
   return loaderPath
+}
+
+function isSamePath(x: string, y: string) {
+  return x.replace(/\\/g, '/').toLowerCase() === y.replace(/\\/g, '/').toLowerCase()
+}
+
+function isSamePackage(x: string, y: string) {
+  if (path.basename(x) === path.basename(y)) {
+    const xName = getPackageName(x)
+    const yName = getPackageName(y)
+    if (!xName || !yName) {
+      return false
+    }
+    return xName === yName
+  }
+  return false
 }
